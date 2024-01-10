5b14031183dcd452fcad9d3a4c0300b819b568f4
Alejandro Palarich <duxducisarsen@gmail.com>
2024-01-09 22:16:07 -0300

    Avanzo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  
   devtools: { enabled: false },
 
-  css: [
-    'bootstrap/dist/css/bootstrap.css',
-  ]
+  css: ['bootstrap/dist/css/bootstrap.css', '~/assets/css/main.css']
+
+
 })
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div class="container-fluid bg-gradient text-white" style=" height: 100vh;
+                    background-color:rgb(30,130,210); font-family: jetbrain-bold;">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <h1 style="font-size: calc(5rem + 1.5vw); color: rgb(61,61,61)">IceO</h1>
+                    <br>
+                    <h1>Gestionar tu empresa va a ser un juego de niños</h1>
+                    <h1>ERP 4.0</h1>
+                    <br>
+                    <br>
+                    <br>
+                    (Nos estamos renovando)
+                </div>
+            </div>
+        </div>
+        <!-- <NuxtWelcome /> -->
+    </div>
+</template>
