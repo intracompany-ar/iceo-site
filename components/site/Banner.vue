4340cfb5d0b1b52a148d7da814f6451935db9b8d
Alejandro Palarich <duxducisarsen@gmail.com>
2024-01-21 03:36:39 -0300

    Agrego logo

--- components/site/Banner.vue
+++ components/site/Banner.vue
@@ -5,9 +5,9 @@
 <template>
 	<div class="tw-p-8">
 		<div class="tw-flex">
-			<div class="tw-w-full tw-md:w-1/2">
+			<div class="tw-w-full tw-md:w-1/2 tw-mt-8">
 				<h1 class="tw-text-8xl">Ice<span class="tw-font-bold">O</span></h1>
-				<h2 class="tw-text-6xl">El ERP que 
+				<h2 class="tw-text-6xl">El ERP que
 					<div class="tw-font-black tw-text-iceobluearg"> se adapta a vos</div>
 				</h2>
 				<br>
@@ -18,8 +18,9 @@
 					para disfrutar la adrenalina de emprender y arrasar a la competencia.
 				</h1>
 			</div>
-			<div class="tw-w-full tw-md:w-1/2 tw-bg-primary-200">
-				<!-- Logo gigante -->
+			<div class="tw-w-full tw-md:w-1/2">
+				<img src="~/assets/img/logos/IceSoloV3.png" alt="IceO Logo" class="tw-mx-auto" width="500rem"
+					height="500rem">
 			</div>
 		</div>
 
